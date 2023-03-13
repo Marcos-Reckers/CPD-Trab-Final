08f3edc4c7d997beb1c21b2c81e4fa88ecc0b993
Marcos Reckers <marcosqlkr@hotmail.com>
2023-03-13 12:38:27 -0300

    make some changes on GUI layout

--- test.py
+++ test.py
@@ -1,35 +1,42 @@
 import PySimpleGUI as sg
 
 max_price = 500
+block_column_size = 15
+block_line_size = 10
+
 
 layout = [
     [
         sg.Text('STEAM BUG'),
     ],
     [
-        sg.Text('GENERO'),sg.Text('IDIOMA'),sg.Text('TAGS'),sg.Text('DATA'),
+        sg.Text('GENERO', justification= "center", size= block_column_size),sg.Text('IDIOMA', justification= "center", size= block_column_size),sg.Text('TAGS', justification= "center", size= block_column_size),sg.Text('DATA', justification= "center", size= block_column_size), sg.Text('DEV', justification= "center", size= block_column_size),sg.Text('PUBLISHER', justification= "center", size= block_column_size)
     ],
     [
-        sg.Listbox(["a","b","c","d"], size=(15, 15), key='-GENERO-'),
-        sg.Listbox(["a","b","c","d"], size=(15, 15), key='-IDIOMA-'),
-        sg.Listbox(["a","b","c","d"], size=(15, 15), key='-TAGS-'),
-        sg.Listbox(list(range(1960, 2022)), size=(15, 15), key='-DATA-')
+        sg.Listbox(["a","b","c","d"], size=(block_column_size, block_line_size), key='-GENERO-'),
+        sg.Listbox(["a","b","c","d"], size=(block_column_size, block_line_size), key='-IDIOMA-'),
+        sg.Listbox(["a","b","c","d"], size=(block_column_size, block_line_size), key='-TAGS-'),
+        sg.Listbox(list(range(1960, 2022)), size=(block_column_size, block_line_size), key='-DATA-'),
+        sg.Listbox(["a","b","c","d"], size=(block_column_size, block_line_size), key='-DEV-'),
+        sg.Listbox(["a","b","c","d"], size=(block_column_size, block_line_size), key='-PUBLISHER-')
     ],
     [
-        sg.Text('DEV'),sg.Text('PUBLISHER')
+        sg.Push(),sg.Text('Preço MIN:', justification= "center"),sg.Push(), sg.Text('Preço MAX:', justification= "center")
     ],
     [
-        sg.Listbox(["a","b","c","d"], size=(15, 15), key='-DEV-'),
-        sg.Listbox(["a","b","c","d"], size=(15, 15), key='-PUBLISHER-')
+        sg.Slider(orientation="h",range=(0,max_price),resolution=0.01,key='-PRICE_MIN-',size=(60, 10)), sg.Slider(orientation="h",range=(0,max_price),resolution=0.01,default_value=max_price,key='-PRICE_MAX-')
     ],
     [
-        sg.Text('Preço MIN:'), sg.Text('Preço MAX:')
-    ],
-    [
-        sg.Slider(orientation="h",range=(0,max_price),resolution=0.01,key='-PRICE_MIN-'), sg.Slider(orientation="h",range=(0,max_price),resolution=0.01,default_value=max_price,key='-PRICE_MAX-')
-    ],
-    [
-        sg.Text('Review:'),sg.Checkbox('Extremamente Positivas'),sg.Checkbox('Muito Positivas'),sg.Checkbox('Positivas'),sg.Checkbox('Ligeiramente Positivas'),sg.Checkbox('Neutras'),sg.Checkbox('Ligeiramente Negativas'),sg.Checkbox('Negativas'),sg.Checkbox('Muito Negativas'),sg.Checkbox('Extremamente Negativas')
+        [sg.Text('Review:')],
+        [
+            sg.Checkbox('Extremamente Positivas', size=(block_column_size * 2, 1)),sg.Checkbox('Muito Positivas', size=(block_column_size * 2, 1)),sg.Checkbox('Positivas', size=(block_column_size * 2, 1))
+        ],
+        [
+            sg.Checkbox('Ligeiramente Positivas' , size=(block_column_size * 2, 1)),sg.Checkbox('Neutras', size=(block_column_size * 2, 1)),sg.Checkbox('Ligeiramente Negativas', size=(block_column_size * 2, 1))
+        ],
+        [
+            sg.Checkbox('Negativas', size=(block_column_size * 2, 1)),sg.Checkbox('Muito Negativas', size=(block_column_size * 2, 1)),sg.Checkbox('Extremamente Negativas', size=(block_column_size * 2, 1))
+        ]
     ],
     [
         sg.Text('AppID:'), sg.InputText(key='-ID-'), sg.Text('Nome:'), sg.InputText(key='-NAME-')
