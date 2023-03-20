c8a7998dba33232c570abf5197019021d3d099d1
Ian Kersz <kerszamaral@gmail.com>
2023-03-20 20:06:06 -0300

    Removed B+ trees from branch

--- code/classes/Trees/b+.hpp
+++ code/classes/Trees/b+.hpp
@@ -7,36 +7,6 @@
 
 namespace Trees
 {
-    const int MAX = 4096; // size of each node
-    class BPlus
-    {
-    private:
-        class Node
-        {
-        private:
-            bool IS_LEAF;
-            int *key, size;
-            Node **ptr;
-            friend class BPlus;
-
-        public:
-            Node();
-        };
-
-        Node *root;
-        void insertInternal(int, Node *, Node *);
-        void removeInternal(int, Node *, Node *);
-        Node *findParent(Node *, Node *);
-
-    public:
-        BPlus();
-        void search(int);
-        void insert(int);
-        void remove(int);
-        void display(Node *);
-        Node *getRoot();
-        void cleanUp(Node *);
-        ~BPlus();
-    };
+    
 }
 
